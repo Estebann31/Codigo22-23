6deb43402fe3fab0cc897f5578113cd0d2ecfa05
estheralmendross <esther.almendros@alu.uclm.es>
2023-03-22 22:43:30 +0100

    Atributos contenedor

--- src/Contenedor.java
+++ src/Contenedor.java
@@ -1,3 +1,15 @@
+
 public class Contenedor {
+
+    private int identificador;
+    private int peso;
+    private String pais;
+    private boolean aduanas;
+    private int prioridad;
+    private String descripcion_contenido;
+    private String empresa_que_envia;
+    private String empresa_que_recibe;
+
+
 
 }
