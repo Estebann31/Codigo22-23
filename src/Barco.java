18f0ca0408c4032723452d66365dc9470ea0b359
Celia <celia.almendros@alu.uclm.es>
2023-03-22 22:46:42 +0100

    getter setter

--- src/Barco.java
+++ src/Barco.java
@@ -1,4 +1,10 @@
 public class Barco {
     Contenedor[][] barco = new Contenedor[10][12];
-
+    public String toString(){
+        for(int f=0; f<10; f++){
+            for(int c=0; c<12;c++){
+                if()
+            }
+        }
+    }
 }
