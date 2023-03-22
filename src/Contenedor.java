4ee3eaef698a29dd6912aa3f1563957e0f1e0204
estheralmendross <esther.almendros@alu.uclm.es>
2023-03-22 22:51:43 +0100

    Constructor contenedor

--- src/Contenedor.java
+++ src/Contenedor.java
@@ -10,6 +10,17 @@ public class Contenedor {
     private String empresa_que_envia;
     private String empresa_que_recibe;
 
+    public Contenedor(int identificador, int peso, String pais, boolean aduanas, int prioridad, String descripcion_contenido, String empresa_que_envia, String empresa_que_recibe ){
+        this.identificador = identificador;
+        this.peso = peso;
+        this.pais = pais;
+        this.aduanas = aduanas;
+        this.prioridad= prioridad;
+        this.descripcion_contenido = descripcion_contenido;
+        this.empresa_que_envia = empresa_que_envia;
+        this.empresa_que_recibe = empresa_que_recibe;
+    }
+
 
 
 }
