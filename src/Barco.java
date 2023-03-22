74bd3023a327ca915e47d2b3fcde24c7e125801a
Celia <celia.almendros@alu.uclm.es>
2023-03-22 22:49:30 +0100

    barco

--- src/Barco.java
+++ src/Barco.java
@@ -1,10 +1,6 @@
 public class Barco {
-    Contenedor[][] barco = new Contenedor[10][12];
-    public String toString(){
-        for(int f=0; f<10; f++){
-            for(int c=0; c<12;c++){
-                if()
-            }
-        }
-    }
+    public static final int FILAS = 10;
+    public static final int COLUMNAS = 12;
+    Contenedor[][] contenedor = new Contenedor[FILAS][COLUMNAS];
+
 }
